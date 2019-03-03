e3146e0765432057e52d6acd0b2c0fa91b15ec47
Faith <pfaithmtan@gmail.com>
2019-03-02 18:31:26 -0800

    Revert "revert greeting"
    This reverts commit 9d4c44520fb5a53b188af4f6f4d634548d7bd526.

--- hello/views.py
+++ hello/views.py
@@ -13,9 +13,5 @@ def index(request):
 
 def db(request):
 
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, "db.html", {"greetings": greetings})
+    rooms = Room.objects.all()
+    return render(request, "db.html", {"rooms": rooms})
