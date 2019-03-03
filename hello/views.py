4a2877bcc3369bef566ba40f252ef164ff56ce1c
Faith <pfaithmtan@gmail.com>
2019-03-02 18:52:07 -0800

    remove comma and changed index

--- hello/views.py
+++ hello/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
-from .models import Greeting, Room
+from .models import Greeting
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    # return render(request, "index.html")
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    return render(request, "index.html")
 
 def db(request):
 
-    rooms = Room.objects.all()
-    return render(request, "db.html", {"rooms": rooms})
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, "db.html", {"greetings": greetings})
